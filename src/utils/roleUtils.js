be5eb4e16f10a4247c3f04d78c0ea8e978f83beb
dogukanngurol <dogukanngurol@gmail.com>
2025-12-22 19:54:05 +0300

    Update roleUtils.js

--- src/utils/roleUtils.js
+++ src/utils/roleUtils.js
@@ -1,1 +1,12 @@
+export const ROLES = {
+  ADMIN: 'Admin',
+  MANAGER: 'Yönetici',
+  DIRECTOR: 'Müdür',
+  FIELD: 'Saha Personeli'
+};
 
+export const PERMISSIONS = {
+  VIEW_REPORTS: [ROLES.ADMIN, ROLES.DIRECTOR],
+  MANAGE_USERS: [ROLES.ADMIN],
+  ASSIGN_JOBS: [ROLES.ADMIN, ROLES.MANAGER],
+};
